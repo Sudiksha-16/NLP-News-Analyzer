ce5e5d714c61b9650999280b3211a461bb3d1a3b
Sudiksha <162961672+Sudiksha-16@users.noreply.github.com>
2025-08-04 18:55:10 +0530

    Commit

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import InputForm from './components/InputForm';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <InputForm />
+    </div>
+  );
+}
+
+export default App;
